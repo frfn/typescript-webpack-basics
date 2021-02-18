f97e6f81a7806d7c866054709aefb4b09c75b640
frfn <flexer.fadrigalan@gmail.com>
2021-02-18 14:32:08 -0500

    added babel-loader plus babel deps | .babelrc, etc

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,28 @@
+/* creating a class */
+// this is understood
+
+/* class Dog {
+	constructor(private _name: string) {}
+
+	get name() {
+		return this._name;
+	}
+}
+const oscar = new Dog("Dog Name: Oscar"); */
+// console.log(returnAString(oscar.name));
+
+// this class declaration is not understood by babel UNTIL you enable the plugin
+export class A {
+	greeting = "Hello World";
+}
+
 const returnAString = (someStr: string): string => {
 	return someStr;
 };
 
-alert(returnAString("I am running a TS fn!"));
+const l = (someStr: string) => {
+	console.log(someStr);
+};
+
+console.log(new A().greeting);
+l(returnAString("1"));
